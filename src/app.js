d40e4e9c4df9801bc048072bbe65da8eb63c8535
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-02-20 14:18:00 +0100

    :lipstick: Add style to index.html

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+const connectBtn = document.querySelector('#connect');
+const accountInput = document.querySelector('#accountNumber');
+const checkBalanceBtn = document.querySelector('#checkBalance');
+const displayBalance = document.querySelector('#balance');
+const amount = document.querySelector('#amount');
+const toAccountInput = document.querySelector('#toAccount');
+const sendBtn = document.querySelector('#sendTrx');
+const listTransactions = document.querySelector('#transactions');
+
 const rpc = new Web3(
   'https://sepolia.infura.io/v3/808b64a9d57e4dfc93f8f6de1ccd5739'
 );
@@ -6,4 +15,14 @@ function initApp() {
   console.log(rpc);
 }
 
+async function connectWallet() {
+  try {
+    console.log(await ethereum.request({ method: 'eth_requestAccounts' }));
+  } catch (error) {
+    throw new Error('User denied account access...', error);
+    alert('You need to connect your wallet to use this app');
+  }
+}
+
 document.addEventListener('DOMContentLoaded', initApp);
+connectBtn.addEventListener('click', connectWallet);
