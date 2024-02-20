9794b41a130f6d5e32cad3bd09a74a927e8b317c
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-02-20 18:14:00 +0100

    :sparkles: Add checkBalance function and improve style

--- src/app.js
+++ src/app.js
@@ -11,18 +11,45 @@ const rpc = new Web3(
   'https://sepolia.infura.io/v3/808b64a9d57e4dfc93f8f6de1ccd5739'
 );
 
+let accounts;
+
 function initApp() {
   console.log(rpc);
 }
 
 async function connectWallet() {
   try {
-    console.log(await ethereum.request({ method: 'eth_requestAccounts' }));
+    if (typeof ethereum !== 'undefined') {
+      accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    } else {
+      alert('You need to install MetaMask to use this app');
+      return;
+    }
   } catch (error) {
     throw new Error('User denied account access...', error);
-    alert('You need to connect your wallet to use this app');
+  }
+}
+
+async function checkBalance() {
+  try {
+    if (typeof ethereum !== undefined) {
+      accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+      const balance = await ethereum.request({
+        method: 'eth_getBalance',
+        params: [accountInput.value, 'latest'],
+      });
+
+      const parsedBalance = parseInt(balance) / Math.pow(10, 18);
+      displayBalance.innerHTML = parsedBalance.toFixed(2) + ' ETH';
+    } else {
+      throw new Error('Account not found');
+    }
+  } catch (error) {
+    error.message = 'Error checking balance';
   }
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
 connectBtn.addEventListener('click', connectWallet);
+checkBalanceBtn.addEventListener('click', checkBalance);
